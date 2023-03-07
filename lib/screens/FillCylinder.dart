197dc67e5d70c37830beac8383f8cdf186235e7a
Sufiyankallada <sufiyankallada@gmail.com>
2023-03-07 16:39:45 +0530

    third commit , added homepage.dart and FillCylinder.dart
    removed unnecessary code from main.dart

---
+++ lib/screens/FillCylinder.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: file_names, duplicate_ignore
+
+import 'package:flutter/material.dart';
+
+class FillCylinder extends StatefulWidget {
+  const FillCylinder({super.key});
+
+  @override
+  State<FillCylinder> createState() => _FillCylinderState();
+}
+
+class _FillCylinderState extends State<FillCylinder> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Fills Cylinder"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("TAP TO SCAN"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
