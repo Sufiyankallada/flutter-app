ec8598f9fe4be63feac6108217be632490ef1d7a
Sufiyankallada <sufiyankallada@gmail.com>
2023-03-07 17:24:50 +0530

    added qr code scanning in fill cylinder

--- lib/screens/FillCylinder.dart
+++ lib/screens/FillCylinder.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: file_names, duplicate_ignore
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class FillCylinder extends StatefulWidget {
   const FillCylinder({super.key});
@@ -8,6 +10,10 @@ class FillCylinder extends StatefulWidget {
   @override
   State<FillCylinder> createState() => _FillCylinderState();
 }
+
+String getresult = "-1";
+
+List barcodeList = [];
 
 class _FillCylinderState extends State<FillCylinder> {
   @override
@@ -20,10 +26,63 @@ class _FillCylinderState extends State<FillCylinder> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            ElevatedButton(onPressed: () {}, child: const Text("TAP TO SCAN")),
+            ElevatedButton(
+              onPressed: () {
+                scanQRCode();
+              },
+              child: const Text("TAP TO SCAN"),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: barcodeList.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      ListTile(
+                        title: Text(barcodeList[index]),
+                        subtitle: Text(index.toString()),
+                        tileColor: Colors.grey[350],
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      )
+                    ],
+                  );
+                })
           ],
         ),
       ),
     );
+  }
+
+  Future scanQRCode() async {
+    try {
+      FlutterBarcodeScanner.getBarcodeStreamReceiver(
+              '#ff6666', 'Cancel', true, ScanMode.QR)!
+          .listen((event) => setState(() {
+                if (!mounted) return;
+                if (!barcodeList.contains(event)) {
+                  barcodeList.add(event);
+                }
+              }));
+
+      if (!mounted) return;
+
+      // setState(() {
+      //   getresult = qr;
+      //   dtLst.add(getresult);
+      // });
+    } on PlatformException {
+      setState(() {
+        getresult = 'Failed to scan QR Code.';
+      });
+    }
   }
 }
